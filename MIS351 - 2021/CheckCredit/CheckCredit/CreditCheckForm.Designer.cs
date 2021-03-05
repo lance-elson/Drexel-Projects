2166fb56ca8638ae8b9cafa03a5ee1642cb41ae1
Lance Elson <64926943+lance-elson@users.noreply.github.com>
2021-03-05 15:07:35 -0500

    Add files via upload

---
+++ MIS351 - 2021/CheckCredit/CheckCredit/CreditCheckForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace CheckCredit
+{
+    partial class CreditCheckForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textboxPrice = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter purchase price:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(12, 62);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 13);
+            this.labelOutput.TabIndex = 1;
+            // 
+            // textboxPrice
+            // 
+            this.textboxPrice.Location = new System.Drawing.Point(126, 6);
+            this.textboxPrice.Name = "textboxPrice";
+            this.textboxPrice.Size = new System.Drawing.Size(100, 20);
+            this.textboxPrice.TabIndex = 2;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(126, 32);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(100, 23);
+            this.buttonEnter.TabIndex = 3;
+            this.buttonEnter.Text = "Enter";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // CreditCheckForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(242, 100);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textboxPrice);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label1);
+            this.Name = "CreditCheckForm";
+            this.Text = "Check Credit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textboxPrice;
+        private System.Windows.Forms.Button buttonEnter;
+    }
+}
+
---
+++ MIS351 - 2021/CheckCredit/CheckCredit/CreditCheckForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Lance Elson
+namespace CheckCredit
+{
+    public partial class CreditCheckForm : Form
+    {
+        public CreditCheckForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+
+                double price = double.Parse(textboxPrice.Text);
+                const double creditLimit = 8000;
+
+
+                if (price > creditLimit)
+                {
+                    labelOutput.Text = ("You have exceeded the credit limit of " +
+                        creditLimit.ToString());
+                }
+
+                else
+                {
+                    labelOutput.Text = "Approved";
+                }
+           
+        }
+    }
+}
